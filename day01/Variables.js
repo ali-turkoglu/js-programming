fb25f9ff73670984aa0709f941536b5b28145d9b
ali-turkoglu <aliturkoglu@gmail.com>
2025-10-23 01:47:09 +0200

    Intial commit

---
+++ day01/Variables.js
@@ -0,0 +1,40 @@
+let studentName = "James";
+let studentAge = 25;
+let isFullTime = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isFullTime);
+
+studentName = "Mykola";
+console.log(studentName);
+
+//console.log(typeof studentName);
+/*
+Comment 1
+Comment 2
+*/
+
+console.log("-------------------------------------");
+
+if (true) {
+  //var a =100;
+  let a = 100;
+}
+//console.log(a);
+
+console.log("-----------------------------------");
+
+let x = 300;
+//let x = 500;
+
+console.log("------------------------------------");
+
+const PI = 3.14; //unchangable
+
+console.log(PI);
+
+console.log("----------------------------------");
+
+const MAXIMUM_LIMIT = 10000;
+console.log(MAXIMUM_LIMIT);
