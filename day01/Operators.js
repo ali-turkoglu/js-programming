5f63b89d574a474d761077f87100650f1a087d43
ali-turkoglu <aliturkoglu@gmail.com>
2025-10-23 16:25:10 +0200

    Operators

---
+++ day01/Operators.js
@@ -0,0 +1,47 @@
+// Aritmetic Operators
+
+console.log(10 + 20);
+console.log(100 - 20);
+console.log(100 / 30);
+console.log(10 * 20);
+console.log(10 % 3);
+
+console.log("------------------------------------");
+
+let x;
+console.log(x);
+x = 100;
+console.log(x);
+x += 500;
+console.log(x);
+
+console.log("-------------------------------------");
+
+console.log(100>20);    //true
+
+console.log(100<20);    //false
+
+console.log(10>=10);    //true
+
+console.log(100==100);  //true
+
+console.log(100==="100"); //false
+console.log(100=="100"); //true
+
+console.log(100!=90);   //true
+console.log(100!=100);   //false
+console.log(100!=="100");   //true
+
+console.log("------------------------------------");
+
+console.log(true && true); //true
+console.log(true && false); //false
+
+console.log(true || false); //true
+console.log(false || true); //true
+
+console.log(!true);  //false
+
+
+
+
